9ee6a8529a6b31022fc5b5403f2820ac7123ba7f
chawije2002 <57843322+chawije2002@users.noreply.github.com>
2022-04-01 22:03:00 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,30 @@
+n1=int(input("enter number 1 :"))
+n2=int(input("enter number 2 :"))
+count=0
+
+if n2 > n1:
+    print("Prime numbers between {0} and {1} is: ".format(n1,n2))
+    for num in range(n1, n2+1):
+        if num > 1:
+            isDivisible = False;
+            for x in range(2, num):
+                if num % x == 0:
+                    isDivisible = True;
+            if not isDivisible:
+               print(num)
+               count += 1;
+
+else:
+    print("Prime numbers between {0} and {1} is: ".format(n2,n1))
+    for num in range(n2, n1+1):
+        if num > 1:
+            isDivisible = False;
+            for x in range(2, num):
+                if num % x == 0:
+                    isDivisible = True;
+            if not isDivisible:
+               print(num)
+               count += 1;
+
+print("Total Prime numbers: {}".format(count))
+input("Are u satisfied?(y/n)")
